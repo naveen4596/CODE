81992e9215c94980919e0ed89e96474d5274912a
root <root@ip-172-31-25-123.us-west-1.compute.internal>
2023-02-04 10:38:24 +0000

    main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,31 @@
+provider "aws" {
+region="us-west-1"
+access_key=""
+secret_key= ""
+}
+
+resource "aws_iam_user" "user1"{
+name= "user1"
+}
+
+resource "aws_iam_user" "user2"{
+name= "user2"
+}
+
+resource "aws_iam_user" "user3"{
+name= "user3"
+}
+
+
+
+resource "aws_iam_group" "group"{
+name= "group1"
+}
+
+resource "aws_iam_group_membership" "test"{
+name= "users with grp"
+
+users=[aws_iam_user.user1.name,aws_iam_user.user2.name,aws_iam_user.user3.name]
+group= aws_iam_group.group.name
+}
 
